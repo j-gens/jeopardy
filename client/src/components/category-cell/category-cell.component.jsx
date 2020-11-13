45cd6d1774b467d71794947b4eab188031f2b672
j-gens <jgens2@gmail.com>
2020-11-13 13:16:15 -0800

    Boilerplate functional component and styles sheet

---
+++ client/src/components/category-cell/category-cell.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '/.category-cell.styles.css';
+
+
+const CategoryCell = () => (
+  <div className='category-cell'>
+    category cell
+  </div>
+);
+
+
+export default CategoryCell;
