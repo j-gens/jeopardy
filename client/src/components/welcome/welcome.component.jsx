b3daf8ac908bcd3eab6bd764cf699d71e6d3b5d9
j-gens <jgens2@gmail.com>
2020-11-13 14:35:50 -0800

    Initial layout for welcome component

--- client/src/components/welcome/welcome.component.jsx
+++ client/src/components/welcome/welcome.component.jsx
@@ -3,11 +3,27 @@ import React from 'react';
 import './welcome.styles.css';
 
 
-const Welcome = () => (
-  <div className='welcome'>
-    welcome
-  </div>
-);
+const Welcome = (props) => {
+  return (
+    <div className='welcome'>
+      <div className='welcome--message'>
+        Welcome to Jeopardy!  Choose an option below:
+      </div>
+      <div className='welcome--options'>
+        <div className='welcome--option'>
+          <h3>
+            Start New Game
+          </h3>
+        </div>
+        <div className='welcome--option'>
+          <h3>
+            Join Current Game
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 
 export default Welcome;
