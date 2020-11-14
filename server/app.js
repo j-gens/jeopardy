5fc19d71b3cc4a4a2f629cf21e99aa50fa3c479f
j-gens <jgens2@gmail.com>
2020-11-14 12:08:29 -0800

    Process all clues to make uniform

--- server/app.js
+++ server/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const path = require('path');
+const jservice = require('./jservice.js');
+
 
 const app = express();
 
-
 app.use(express.static('client/dist'));
+
+// path for generating a new game
+app.get('/api/newgame/', (req, res) => {
+  jservice.getGameCategories((error, categories) => {
+    if (error) res.sendStatus(404);
+    jservice.getGameClues(categories, (error, clues) => {
+      if (error) res.sendStatus(404);
+      console.log(req.query.double);
+      jservice.makeConsistentClueValues(clues, req.query.double, (result) => {
+        res.send(result);
+      })
+    });
+  })
+})
 
 
 module.exports = app;
