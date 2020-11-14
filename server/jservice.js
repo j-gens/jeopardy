9ab6f88dc0119cd392d7f7e8022d547d20e58eeb
j-gens <jgens2@gmail.com>
2020-11-14 12:08:09 -0800

    Add functionality to make clue values uniform for each round

--- server/jservice.js
+++ server/jservice.js
@@ -30,8 +30,23 @@ const getGameClues = (categories, callback) => {
     .catch((error) => callback(error));
 }
 
+// Process clue dollar amount to match single/double Jeopardy values
+const makeConsistentClueValues = (categories, game, callback) => {
+  // Determine if single or double Jeopardy round
+  let values = [100, 200, 300, 400, 500];
+  if (game === 'true') values = [200, 400, 600, 800, 1000];
+  // Iterate through all the clues to update values as needed
+  categories.forEach((category) => {
+    category.forEach((clue, index) => {
+      clue.value = values[index];
+    });
+  });
+  callback(categories);
+}
+
 
 module.exports = {
   getGameCategories,
   getGameClues,
+  makeConsistentClueValues,
 }
