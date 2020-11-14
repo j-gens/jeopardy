d857c4529db4eb96438f6745bd83334738903a98
j-gens <jgens2@gmail.com>
2020-11-14 10:10:33 -0800

    Boilerplate functional component and styles sheet

---
+++ client/src/components/score/score.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './score.styles.css';
+
+
+const Score = () => (
+  <div className='scoreboard--player'>
+    player score
+  </div>
+);
+
+
+export default Score;
