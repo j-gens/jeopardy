f1dd04ede90238af4237e16659edc454fa45b77a
j-gens <jgens2@gmail.com>
2020-11-14 12:09:15 -0800

    Add padding for initial spacing of button options

--- client/src/components/welcome/welcome.component.jsx
+++ client/src/components/welcome/welcome.component.jsx
@@ -3,27 +3,26 @@ import React from 'react';
 import './welcome.styles.css';
 
 
-const Welcome = (props) => {
-  return (
-    <div className='welcome'>
-      <div className='welcome--message'>
-        Welcome to Jeopardy!  Choose an option below:
+const Welcome = ({ generateGame, joinGame }) => (
+  <div className='welcome'>
+    <div className='welcome--message'>
+      Welcome to Jeopardy!  Choose an option below:
+    </div>
+    <div className='welcome--options'>
+      <div className='welcome--option' onClick={generateGame}>
+        <h3>
+          Start New Game
+        </h3>
       </div>
-      <div className='welcome--options'>
-        <div className='welcome--option'>
-          <h3>
-            Start New Game
-          </h3>
-        </div>
-        <div className='welcome--option'>
-          <h3>
-            Join Current Game
-          </h3>
-        </div>
+      <div className='welcome--option' onClick={joinGame}>
+        <h3>
+          Join Current Game
+        </h3>
       </div>
     </div>
-  );
-}
+  </div>
+);
+
 
 
 export default Welcome;
