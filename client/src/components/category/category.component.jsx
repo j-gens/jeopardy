337a3851767735093d5fdfbf4b2cc2d32a29e875
j-gens <jgens2@gmail.com>
2020-11-14 10:13:01 -0800

    Add general layout of title card and clue cards along with width spacing

--- client/src/components/category/category.component.jsx
+++ client/src/components/category/category.component.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import CategoryTitle from '../category-title/category-title.component.jsx';
+import CategoryCell from '../category-cell/category-cell.component.jsx';
 import './category.styles.css';
 
 
-const Category = () => (
+const Category = ({ category }) => (
   <div className='category'>
-    category
+    <CategoryTitle clue={category[0]} />
+    {
+      category.map((clue, index) =>
+        <CategoryCell clue={clue} key={index} />
+      )
+    }
   </div>
 );
 
