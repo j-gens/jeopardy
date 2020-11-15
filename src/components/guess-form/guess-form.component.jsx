c7d9cb3d560d19d7606bddf42d31b0de735c9b18
j-gens <jgens2@gmail.com>
2020-11-15 15:04:15 -0800

    Add functionality and basic styling for players to input their answer

--- src/components/guess-form/guess-form.component.jsx
+++ src/components/guess-form/guess-form.component.jsx
@@ -3,9 +3,51 @@ import React from 'react';
 import './guess-form.styles.css';
 
 
-const GuessForm = () => (
+class GuessForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      guess: '',
+    }
+  }
 
-);
+  handleChange = (event) => this.setState({guess: event.target.value});
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { guess } = this.state;
+    const { checkAnswer } = this.props;
+    checkAnswer(guess);
+
+  }
+
+  render() {
+    const { guess } = this.state;
+    const { player } = this.props;
+    return (
+      <form className='guess-form' onSubmit={this.handleSubmit}>
+        <label htmlFor='guess'>
+          <h4>
+            Player {player}! Who/What/When/Where is....
+          </h4>
+        </label>
+        <input
+          type='text'
+          name='guess'
+          id='guess'
+          value={guess}
+          onChange={this.handleChange}
+        />
+        <button type='submit'>
+          <h3>
+            Submit
+          </h3>
+        </button>
+      </form>
+    );
+  }
+}
 
 
 export default GuessForm;
