227d610aa5ebaf482c5375fb610c7e57062097d8
j-gens <jgens2@gmail.com>
2020-11-15 10:05:27 -0800

    Add random number generator for category ids

--- src/utilities/game-logic.js
+++ src/utilities/game-logic.js
@@ -1,8 +1,17 @@
+// Generate 6 random category IDs to query jService API for clues
+export const generateCategoryIds = () => {
+  const categories = [];
+  for (let i = 0; i < 6; i++) {
+    categories.push(Math.floor(Math.random() * 18000 + 1));
+  }
+  return categories;
+}
+
 // Process clue dollar amount to match single/double Jeopardy values
-export const makeConsistentClueValues = (categories, game, callback) => {
+export const makeConsistentClueValues = (categories, isDouble, callback) => {
   // Determine if single or double Jeopardy round
   let values = [100, 200, 300, 400, 500];
-  if (game === 'true') values = [200, 400, 600, 800, 1000];
+  if (isDouble) values = [200, 400, 600, 800, 1000];
   // Iterate through all the clues to update values as needed
   categories.forEach((category) => {
     category.forEach((clue, index) => {
