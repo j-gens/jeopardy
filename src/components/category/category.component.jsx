26e27b1680ed629fa82068a1f79da839a7cfcedf
j-gens <jgens2@gmail.com>
2020-11-15 11:35:47 -0800

    Pass along method for handling click to category cells

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -5,12 +5,12 @@ import CategoryCell from '../category-cell/category-cell.component.jsx';
 import './category.styles.css';
 
 
-const Category = ({ category }) => (
+const Category = ({ category, displayClue }) => (
   <div className='category'>
     <CategoryTitle clue={category[0]} />
     {
       category.map((clue, index) =>
-        <CategoryCell clue={clue} key={index} />
+        <CategoryCell clue={clue} displayClue={displayClue} key={index} />
       )
     }
   </div>
