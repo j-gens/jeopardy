94e2ee31cddafa35726168d5b75e47b9866dbdb8
j-gens <jgens2@gmail.com>
2020-11-15 11:37:36 -0800

    Handle click event by passing back prop details and removing value display

--- src/components/category-cell/category-cell.component.jsx
+++ src/components/category-cell/category-cell.component.jsx
@@ -7,17 +7,30 @@ class CategoryCell extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicked: false
-    };
+      clicked: false,
+    }
+  }
+
+  handleClick = () => {
+    const { clicked } = this.state;
+    const { displayClue, clue: { question, answer, value } } = this.props;
+    // If previously clicked on, do nothing
+    if (clicked) return;
+    // Else pass clue details back up to Board component
+    this.setState({ clicked: true });
+    displayClue(question, answer, value);
   }
 
   render() {
-    const { clue: { answer, question, value } } = this.props;
+    const { clicked } = this.state;
+    const { clue: { value } } = this.props;
     return (
-      <div className='category-cell'>
-        <h1>
-          ${value}
-        </h1>
+      <div className='category-cell' onClick={this.handleClick}>
+          {
+            clicked ?
+            ' ' :
+            (<h1>${value}</h1>)
+          }
       </div>
     );
   }
