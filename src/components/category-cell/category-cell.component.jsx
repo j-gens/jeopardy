5ba07d11d04a08ad5d05cc315551abd83ca1e1b6
j-gens <jgens2@gmail.com>
2020-11-16 13:33:50 -0800

    Update CategoryCell component display based on prop display  property value

--- src/components/category-cell/category-cell.component.jsx
+++ src/components/category-cell/category-cell.component.jsx
@@ -8,28 +8,33 @@ class CategoryCell extends React.Component {
     super(props);
     this.state = {
       clicked: false,
-    }
+    };
+  }
+
+  componentDidMount() {
+    const { clue: { display } } = this.props;
+    this.setState({ clicked: !display });
   }
 
   handleClick = () => {
     const { clicked } = this.state;
-    const { displayClue, clue: { question, answer, value } } = this.props;
+    const { displayClue, clueIndex, categoryIndex, updateClueDisplay, clue } = this.props;
+    const { question, answer, value } = clue;
     // If previously clicked on, do nothing
     if (clicked) return;
     // Else pass clue details back up to Board component
-    this.setState({ clicked: true });
+    updateClueDisplay(categoryIndex, clueIndex);
     displayClue(question, answer, value);
   }
 
   render() {
-    const { clicked } = this.state;
-    const { clue: { value } } = this.props;
+    const { clue: { value, display } } = this.props;
     return (
       <div className='category-cell' onClick={this.handleClick}>
           {
-            clicked ?
-            ' ' :
-            (<h1>${value}</h1>)
+            display ?
+            (<h1>${value}</h1>) :
+            ' '
           }
       </div>
     );
