17b3090b6accaeddd7786cbb996eadffbae689d7
j-gens <jgens2@gmail.com>
2020-11-16 11:28:42 -0800

    Pass along category index, clue index, and cell display method to category-cell component

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -5,12 +5,17 @@ import CategoryCell from '../category-cell/category-cell.component.jsx';
 import './category.styles.css';
 
 
-const Category = ({ category, displayClue }) => (
+const Category = ({ category, ...otherProps }) => (
   <div className='category'>
     <CategoryTitle clue={category[0]} />
     {
       category.map((clue, index) =>
-        <CategoryCell clue={clue} displayClue={displayClue} key={index} />
+        <CategoryCell
+          clue={clue}
+          clueIndex={index}
+          {...otherProps}
+          key={index}
+        />
       )
     }
   </div>
