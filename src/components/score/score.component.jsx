56a94c675feef7139c0d6104eb741debdec9b708
j-gens <jgens2@gmail.com>
2020-11-16 15:33:59 -0800

    Add key binding reminder for players

--- src/components/score/score.component.jsx
+++ src/components/score/score.component.jsx
@@ -3,36 +3,25 @@ import React from 'react';
 import './score.styles.css';
 
 
-class Score extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'Enter Name'
-    }
-  }
-
-  changeName = () => {
-    let name = prompt('Please Enter Name:', "Player");
-    if (name.length > 0) this.setState({ name })
-  }
-
-  render() {
-    const { score } = this.props;
-    const { name } = this.state;
-    return (
-      <div className='player'>
-        <div className='player-score'>
-          <h4>
-          {score < 0 ? '-' : ''} ${Math.abs(score)}
-          </h4>
-        </div>
-        <div className='player-name' onClick={this.changeName}>
-          {name}
-        </div>
-      </div>
-    );
-  }
-}
+const Score = ({ score, player }) => (
+  <div className='player'>
+    <div className='player-score'>
+      <h4>
+      {score < 0 ? '-' : ''} ${Math.abs(score)}
+      </h4>
+    </div>
+    <div className='player-name'>
+      Player {player + 1}
+    </div>
+    <div className='player-instructions'>
+      {
+        player === 0 ?
+        'Buzz in with \'z\' key!' :
+        'Buzz in with \'/\' key!'
+      }
+    </div>
+  </div>
+);
 
 
 export default Score;
