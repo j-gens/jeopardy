5fd1bb1aec7f5785d3a14e02392d921e7a15d02e
j-gens <jgens2@gmail.com>
2020-11-16 16:12:55 -0800

    Boilerplate class component and styles sheet

---
+++ src/components/clue-timer/clue-timer.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './clue-timer.styles.css';
+
+
+class ClueTimer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    <div>
+      timer
+    </div>
+  }
+}
+
+
+export default ClueTimer;
